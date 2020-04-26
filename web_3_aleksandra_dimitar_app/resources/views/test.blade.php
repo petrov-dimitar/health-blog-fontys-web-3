00e7a902aec88039e74ffa6ae8689ae746a75c6b
Petrov,Dimitar D.D <dimitar.petrov@student.fontys.nl>
2020-04-26 14:13:43 +0300

    Added new rout and changed default page.

---
+++ web_3_aleksandra_dimitar_app/resources/views/test.blade.php
@@ -0,0 +1,1 @@
+<h1> This Testing page</h1>
--- web_3_aleksandra_dimitar_app/routes/web.php
+++ web_3_aleksandra_dimitar_app/routes/web.php
@@ -12,5 +12,9 @@
 */
 
 Route::get('/', function () {
+    return view('test');
+});
+
+Route::get('/welcome', function () {
     return view('welcome');
 });
