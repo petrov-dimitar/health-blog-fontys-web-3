b8975fa737932e6e3d4cb97d880b682446fa460b
Belcheva,Aleks A.E <a.belcheva@student.fontys.nl>
2020-05-17 10:33:38 +0200

    improved edit profile page

--- health_blog_aleksandra_dimitar_v6/resources/views/editProfile.blade.php
+++ health_blog_aleksandra_dimitar_v6/resources/views/editProfile.blade.php
@@ -6,18 +6,19 @@
     <p>This is the current user:</p>
     {{-- <p>{{auth()->user()}}</p> --}}
     
-    <form method="POST" action="/user/profile/edit/{{auth()->user()->id}}">
+    
+        <p> Id: {{auth()->user()->id}}</p>
+        <p>Email: {{auth()->user()->email}}</p>
+        <p>Name: {{auth()->user()-> name}}</p>
+        <p>Created at: {{auth()->user()->created_at}}</p>
+
+        <form method="POST" action="/user/profile/edit/{{auth()->user()->id}}">
         @csrf
         @method('PUT')
 
         <textarea name="name">Name: {{auth()->user()-> name}}</textarea>
         <button> UPDATE</button>
     </form>
-    
-        <textarea> Id: {{auth()->user()->id}}</textarea>
-        <textarea>Email: {{auth()->user()->email}}</textarea>
-        <textarea>Name: {{auth()->user()-> name}}</textarea>
-        <textarea>Created at: {{auth()->user()->created_at}}</textarea>
    </div>
 @endsection
 
